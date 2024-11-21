f1fc49995069110333ea973ff5b152deca542c2d
vinita132r <vinitar132@gmail.com>
2024-11-21 10:17:47 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import './Components/Homepgsty.css'
+import Mainhome from './Components/Mainhome.jsx';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Mainhome/>}/>
+        </Routes>
+      </BrowserRouter>
       
     </div>
   );
